f6c69f64319acd255544fbea509bb62bda605be8
shenando <shenandoah.grace@gmail.com>
2021-05-13 12:20:53 -0400

    add code kata

---
+++ filter-the-number/js/main.js
@@ -0,0 +1,17 @@
+/*
+Filter the number
+by matt c
+
+Oh no! The number has been mixed up with the text. Your goal is to retreive the number from the text, can you return the number back to it's original state?
+
+Task
+Your task is to return a number from a string.
+
+Details
+You will be given a string of numbers and letters mixed up, you have to return all the numbers in that string in the order they occur.
+*/
+
+var FilterString = function(value) {
+  let num = value.split('').filter(el => +el || el === '0').join('')
+  return Number(num)
+}
