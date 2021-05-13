c47777f52fd82d3fcc59c5022c41fe18f867eb9b
shenando <shenandoah.grace@gmail.com>
2021-05-13 12:28:51 -0400

    add code kata

---
+++ bumps-in-the-road/js/main.js
@@ -0,0 +1,12 @@
+/*
+Bumps in the Road
+by PG1
+
+Your car is old, it breaks easily. The shock absorbers are gone and you think it can handle about 15 more bumps before it dies totally.
+
+Unfortunately for you, your drive is very bumpy! Given a string showing either flat road ("_") or bumps ("n"), work out if you make it home safely. 15 bumps or under, return "Woohoo!", over 15 bumps return "Car Dead".
+*/
+
+function bump(x){
+  return x.split('').filter((el) => el === 'n').length > 15 ? 'Car Dead': 'Woohoo!';
+}
