c277065f6b69633d3511883e853a5e9f74678391
shenando <shenandoah.grace@gmail.com>
2021-05-30 11:52:41 -0400

    add code kata

---
+++ anagram-detector/js/main.js
@@ -0,0 +1,22 @@
+/*
+Anagram Detection
+by user3793153
+
+An anagram is the result of rearranging the letters of a word to produce a new word (see wikipedia).
+
+Note: anagrams are case insensitive
+
+Complete the function to return true if the two arguments given are anagrams of each other; return false otherwise.
+
+Examples
+"foefet" is an anagram of "toffee"
+
+"Buckethead" is an anagram of "DeathCubeK"
+*/
+
+var isAnagram = function(test, original) {
+  let sortedA = test.toLowerCase().split('').sort().join('')
+  let sortedB = original.toLowerCase().split('').sort().join('')
+  
+  return sortedA === sortedB
+};
