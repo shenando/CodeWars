6cc911ae9d79633aab9b65adbb8c227978665254
shenando <shenandoah.grace@gmail.com>
2021-06-17 09:11:47 -0400

    add code kata

---
+++ lost-number-in-number-sequence/js/main.js
@@ -0,0 +1,26 @@
+/*
+Lost number in number sequence
+by evgv
+
+An ordered sequence of numbers from 1 to N is given. One number might have deleted from it, then the remaining numbers were mixed. Find the number that was deleted.
+
+Example:
+
+The starting array sequence is [1,2,3,4,5,6,7,8,9]
+The mixed array with one deleted number is [3,2,4,6,7,8,1,9]
+Your function should return the int 5.
+If no number was deleted from the array and no difference with it, your function should return the int 0.
+
+Note: N may be 1 or less (in the latter case, the first array will be []).
+*/
+function findDeletedNumber(arr, mixArr) {
+  let sorted = mixArr.sort((a,b) => a - b)
+  let newArr = []
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i] !== sorted[i]) {
+      newArr.push(arr[i])
+    }
+  }
+  
+  return arr.length === mixArr.length ? 0 : newArr[0]
+}
