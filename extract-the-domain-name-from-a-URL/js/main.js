e164b8ea6fac4d8b6f489829a755a0605ac089da
shenando <shenandoah.grace@gmail.com>
2021-07-14 09:23:59 -0400

    add code kata

---
+++ extract-the-domain-name-from-a-URL/js/main.js
@@ -0,0 +1,13 @@
+/*
+Extract the domain name from a URL
+by xianpants
+
+Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. For example:
+
+domainName("http://github.com/carbonfive/raygun") == "github" 
+domainName("http://www.zombie-bites.com") == "zombie-bites"
+domainName("https://www.cnet.com") == "cnet"
+*/
+function domainName(url){
+  return url.replace(/^(https?|http):\/\//, '').replace('www.', '').split('.')[0]
+}
