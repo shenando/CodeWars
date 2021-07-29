a39a5d4da962a820f0b738bf5523fec617987c81
shenando <shenandoah.grace@gmail.com>
2021-07-29 08:24:23 -0400

    add code kata

---
+++ middle-me/js/main.js
@@ -0,0 +1,40 @@
+/*
+Middle Me
+by Capocaccia
+
+Write a function that will take a key of X and place it in the middle of Y repeated N times.
+
+Extra challege (not tested): You can complete this with under 70 characters without using regex. Challenge yourself to do this. It wont be best practices but it will work.
+
+Rules:
+
+If X cannot be placed in the middle, return X.
+
+N will always be > 0.
+
+Example:
+
+X = 'A';
+
+Y = '*';
+
+N = 10;
+
+Y repeated N times = '**********';
+
+X in the middle of Y repeated N times = '*****A*****';
+*/
+
+function middleMe(N, X, Y){
+  if (N % 2 !== 0) {
+    return X
+  } else {
+    let arr = []
+    for(let i = 0; i < N/2; i++){
+      arr.push(Y)
+    }
+    let arr2=[...arr]
+    arr.push(X)
+    return arr.concat(arr2).join('')
+  }
+}
