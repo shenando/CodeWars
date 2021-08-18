8b999f842b1f0c56ae81400aad757babc20c168b
shenan <shenandoah.grace@gmail.com>
2021-08-18 19:32:58 -0400

    edit code kata

--- find-screen-size/js/main.js
+++ find-screen-size/js/main.js
@@ -8,6 +8,7 @@ The Challenge
 Given an integer width and a string ratio written as WIDTH:HEIGHT, output the screen dimensions as a string written as WIDTHxHEIGHT.
 
 Note: The calculated height should be represented as an integer. If the height is fractional, truncate it.
+
 */
 
 function findScreenHeight(width, ratio) {
