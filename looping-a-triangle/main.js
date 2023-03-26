09bf116469f27d65aaeea3ca21dc2938bb3cad23
Shenandoah Grace <69861032+shenando@users.noreply.github.com>
2023-03-26 21:27:06 +0000

    final update

--- looping-a-triangle/main.js
+++ looping-a-triangle/main.js
@@ -64,6 +64,8 @@ Passing this string to console.log should show something like this:
 When you have a program that generates this pattern, define a binding size = 8 and change the program so that it works for any size, outputting a grid of the given width and height.
 */
 
+
+
 let amount = 8;
 let row = [];
 let board = [];
